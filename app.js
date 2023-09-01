b19ee461a2dc11a3e611506a6d442f74e9184b3a
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2023-09-01 23:36:40 +0500

    javascript assignment

---
+++ app.js
@@ -0,0 +1,79 @@
+//TODO:Chapter 1 ========///
+
+
+   //   ?Q1
+   
+//    alert("Hello World");
+ 
+   //  ?Q2
+    
+//    alert("Error! Please enter a valid password.");
+     
+   //  ?Q3 
+   
+   //    alert("Welcome to JS Land... \nHappy Coding!");
+
+ //  ?Q4  
+
+//    alert("Welcome to JS Land...");
+//    alert("Happy Coding!");
+
+//  ?Q5
+
+// console.log("Hello... I can run JS through my web browser's console");
+
+
+//TODO: CHAPTER 2========/
+
+// ?Q1
+
+//  var userName;
+
+ // ?Q2
+
+//  var myName = "Abdullah Khamoor";
+
+ // ?Q3
+
+//  var message = "Hello World";
+//  alert(message);
+
+// ?Q4
+
+// alert("20 years old");
+// alert("Certified Mobile Application Development");
+
+// ?Q5
+
+// var c = "PIZZA\nPIZZ\nPIZ\nP";
+// alert(c);
+
+// ?Q6
+
+// var email = "My email address is abdullah.khamoor4@gmail.com";
+// alert(email);
+
+// ?Q7
+
+// var book = "A smarter way to learn JavasScript";
+// alert( "I am trying to learn from the Book " +book);
+
+// ?Q8
+//in index file
+
+// ?Q9
+
+// var d = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(d);
+
+
+
+
+
+
+
+
+
+
+
+    
